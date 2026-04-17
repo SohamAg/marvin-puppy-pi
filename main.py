9ab6612443de080e57fe50bca07c06e33672233d
Javier Rodriguez <124937710+javier-rod316@users.noreply.github.com>
2026-04-17 18:28:07 -0400

    Implement TCP server for wake word detection
    Added a TCP server to handle client connections and wake word detection.

--- main.py
+++ main.py
@@ -2,12 +2,55 @@
 
 import time
 import rclpy
+import socket
+import threading
 
 from actions.actions import PuppyActions
 from inference.inference import get_command
 
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+wakeWord = 1 # Set to 0 if not using wakeword detection
+wake_event = threading.Event()
+
+def start_server(stop_event):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((HOST, PORT))
+    server.listen()
+    server.settimeout(1.0)
+    print("Server is listening for connections...")
+
+    while not stop_event.is_set():
+        try:
+            conn, addr = server.accept()
+            thread = threading.Thread(target=handle_client, args=(conn, addr), daemon=True)
+            thread.start()
+            print(f"New connection added at {addr}")
+        except socket.timeout:
+            continue
+    server.close()
+
+def handle_client(conn, addr):
+    try:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print(f"Data received: {data} ")
+            if data == b"Marv":
+                wake_event.set()
+    except Exception:
+        pass
 
 def main(args=None):
+    stop_signal = threading.Event()
+    server_thread = threading.Thread(target=start_server, args=(stop_signal,), daemon=True)
+    server_thread.start()
+    if wakeWord:
+        wake_event.wait()
+
     rclpy.init(args=args)
     controller = PuppyActions()
 
@@ -45,9 +88,9 @@ def main(args=None):
         controller.stop()
 
     finally:
+        stop_signal.set()
         controller.destroy_node()
         rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
